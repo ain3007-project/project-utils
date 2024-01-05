7a3a8bd4728a527d57b08232485e62566447df0a
osbm <osmanfbayram@gmail.com>
2024-01-06 01:46:55 +0300

    fix the function

--- project_utils/plotting.py
+++ project_utils/plotting.py
@@ -3,15 +3,13 @@ import matplotlib.pyplot as plt
 
 
 
-def plot_image_mask_by_path(image1_path, image2_path, figsize=(10, 5)):
-    def imshow(image, mask):
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=fifgsize)
-    ax1.imshow(plt.imread(image))
+def plot_image_mask_by_path(image_path, mask_path, figsize=(10, 5)):
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=figsize)
+    ax1.imshow(plt.imread(image_path))
     ax1.set_title('Image')
     ax1.axis('off')
-    ax2.imshow(plt.imread(mask))
+    ax2.imshow(plt.imread(mask_path))
     ax2.set_title('Mask')
     ax2.axis('off')
     plt.show()
-
 
